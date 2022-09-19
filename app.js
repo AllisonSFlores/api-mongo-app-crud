90a73244e1624a26166e20d8466d7b1f859bd6d4
Allison <50599762+AllisonSFlores@users.noreply.github.com>
2022-09-18 18:50:43 -0600

    permissons

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Expose-Headers', 'Content-Length');
   res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+  res.header("permissions", ["http://localhost:8100"]);
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   } else {
